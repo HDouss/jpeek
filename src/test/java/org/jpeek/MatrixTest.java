6c5281b5d2693a1674a3e3a86255e7116d0be80a
HDouss <douss.hamdi@gmail.com>
2020-02-18 20:16:13 +0100

    Revert "sync"

--- src/test/java/org/jpeek/MatrixTest.java
+++ src/test/java/org/jpeek/MatrixTest.java
@@ -29,8 +29,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import org.cactoos.text.TextOf;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 import org.llorllale.cactoos.matchers.Assertion;
 
 /**
@@ -47,7 +47,7 @@ public final class MatrixTest {
      */
     private String xml;
 
-    @BeforeEach
+    @Before
     public void setUp() throws IOException {
         final Path output = Files.createTempDirectory("").resolve("x2");
         final Path input = Paths.get(".");
